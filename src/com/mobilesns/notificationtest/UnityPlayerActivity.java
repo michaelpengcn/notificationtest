32736a18e6f16b3ce3cdd5f55722fc02c5680be0
pc <pc@playnetjoy.com>
2015-12-31 14:14:11 +0800

    support weekly repeat mode. not tested yet.

--- src/com/mobilesns/notificationtest/UnityPlayerActivity.java
+++ src/com/mobilesns/notificationtest/UnityPlayerActivity.java
@@ -25,8 +25,10 @@ public class UnityPlayerActivity extends Activity
 	protected UnityPlayer mUnityPlayer; // don't change the name of this variable; referenced from native code
 	// added by cpeng
 	private static Context mContext = null;
-	private static final int INTERVAL		= 1000 * 60 * 60 * 24;// 24h
-	private static final int REQUEST_CODE	= 10001;
+	private static final int INTERVAL_DAILY		= 1000 * 60 * 60 * 24;// 24h
+	private static final int INTERVAL_WEEKLY	= 1000 * 60 * 60 * 24 * 7; // a week 
+	private static final int INTERVAL			= 1000 * 30;// 30 seconds
+	private static final int REQUEST_CODE		= 10001;
 	public static String PushAction = "AlarmAction";
 	// added end
 	
@@ -42,47 +44,50 @@ public class UnityPlayerActivity extends Activity
 		setContentView(mUnityPlayer);
 		mUnityPlayer.requestFocus();
 		mContext = this;
-		
-		//AlarmNotificationReceiver.sendNotify(0, "cpeng hardcode test", this);
 	}
-	public void SetupNotificationMessage1(String message, int hour, int minute, int second, int repeatMode)
-	{
-	}
+	
 	// repeatMode, 0: none, 1: everyday, 2: week
-	public void SetupNotificationMessage(String message, int hour, int minute, int second, int repeatMode)
+	public void SetupNotificationMessage(String message, int hour, int minute, int second, int repeatMode, int id)
     {
 		Log.v("cpeng > ", "SetupNoficationMessage");
 		if (repeatMode == 0) {
+			Log.v("cpeng > ", "SetupNoficationMessage > do not support non-repeat mode yet");
+			return;
 		}
-		else if (repeatMode ==1) {
-			Log.v("cpeng > ", "SetupNoficationMessage > repeatMode: 1");
-			Intent intent = new Intent(getApplicationContext(), AlarmNotificationReceiver.class);
-			intent.setAction(PushAction);
-			intent.putExtra("id", 0);
-			intent.putExtra("msg", message);
-			PendingIntent sender = PendingIntent.getBroadcast(getApplicationContext(),
-					REQUEST_CODE, intent, PendingIntent.FLAG_CANCEL_CURRENT);
-
-			// Schedule the alarm!
-			AlarmManager am = (AlarmManager) getApplicationContext()
-				.getSystemService(Context.ALARM_SERVICE);
-
-			Calendar calendar = Calendar.getInstance();
-			calendar.set(Calendar.HOUR_OF_DAY, hour);
-			calendar.set(Calendar.MINUTE, minute);
-			calendar.set(Calendar.SECOND, second);
-			calendar.set(Calendar.MILLISECOND, 0);
-
-			am.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
-					INTERVAL, sender);
+		
+		int interval = INTERVAL_DAILY;
+		if (repeatMode == 1) {
+			interval = INTERVAL_DAILY;
 		}
-		else if (repeatMode ==2) {
+		else if (repeatMode == 2) {
+			interval = INTERVAL_WEEKLY;
 		}
+		
+		Log.v("cpeng > ", "SetupNoficationMessage > repeatMode: 1");
+		Intent intent = new Intent(getApplicationContext(), AlarmNotificationReceiver.class);
+		intent.setAction(PushAction);
+		intent.putExtra("id", id);
+		intent.putExtra("msg", message);
+		PendingIntent sender = PendingIntent.getBroadcast(getApplicationContext(),
+				REQUEST_CODE, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+		// Schedule the alarm!
+		AlarmManager am = (AlarmManager) getApplicationContext()
+			.getSystemService(Context.ALARM_SERVICE);
+
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(Calendar.HOUR_OF_DAY, hour);
+		calendar.set(Calendar.MINUTE, minute);
+		calendar.set(Calendar.SECOND, second);
+		calendar.set(Calendar.MILLISECOND, 0);
+
+		am.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
+				interval, sender);
     }
 	
-	public void CancelNotification(int index) {
+	public void CancelNotification(int id) {
 		Intent intent = new Intent(getApplicationContext(), AlarmNotificationReceiver.class);
-        PendingIntent sender = PendingIntent.getBroadcast(getApplicationContext(), 0, intent, 0);
+        PendingIntent sender = PendingIntent.getBroadcast(getApplicationContext(), id, intent, 0);
 
         // And cancel the alarm.
         AlarmManager am = (AlarmManager) getSystemService(ALARM_SERVICE);
